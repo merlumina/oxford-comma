b034a90db0668e6a229a19e18cdf98a74c1f5cb8
Liz <lizriderwilson@gmail.com>
2020-12-11 21:46:26 -0500

    commit tree 32bbbd692ee42dbfd33c5df71c797243df0c536c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        return array.join
+    elsif array.length == 2
+        return array.join(" and ")
+    else
+        new_string = ""
+        counter = 0
+        while counter < array.length - 1
+            new_string += array[counter] + ", "
+            counter += 1
+        end
+        new_string += "and " + array[-1]
+        return new_string
+    end
 end
